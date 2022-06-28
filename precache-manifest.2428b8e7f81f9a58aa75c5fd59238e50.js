3767189d0df9a1ec2186270515c6b4d19b57d242
unknown <junaidarshad113@gmail.com>
2022-06-28 04:32:17 -0700

    Updates

---
+++ precache-manifest.2428b8e7f81f9a58aa75c5fd59238e50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2afbd7219821486b10502725907bd1e2",
+    "url": "/junaid_Portfolio/index.html"
+  },
+  {
+    "revision": "96bc7133da76814d9aaa",
+    "url": "/junaid_Portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/junaid_Portfolio/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/junaid_Portfolio/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96bc7133da76814d9aaa",
+    "url": "/junaid_Portfolio/static/js/main.15bb3f68.chunk.js"
+  },
+  {
+    "revision": "bfd2aff8f152e2e066eb",
+    "url": "/junaid_Portfolio/static/js/runtime-main.73150c2e.js"
+  }
+]);
